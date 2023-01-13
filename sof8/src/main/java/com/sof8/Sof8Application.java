fbb0846b9ffc2e839d06e62f65d5ad866c80a0c5
Hyewon <kckhw35@naver.com>
2023-01-13 11:08:07 +0900

    20230113	Hyewon		Create_Project

---
+++ sof8/src/main/java/com/sof8/Sof8Application.java
@@ -0,0 +1,13 @@
+package com.sof8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sof8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sof8Application.class, args);
+	}
+
+}
