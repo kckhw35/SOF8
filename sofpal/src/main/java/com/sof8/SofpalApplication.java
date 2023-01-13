30c4e1b9bfb226338bc50900db21d71d971621cf
Hyewon <kckhw35@naver.com>
2023-01-13 11:41:51 +0900

    20230113	Hyewon		Test project name

---
+++ sofpal/src/main/java/com/sof8/SofpalApplication.java
@@ -0,0 +1,13 @@
+package com.sof8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofpalApplication.class, args);
+	}
+
+}
---
+++ sofpal/src/test/java/com/sof8/SofpalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sof8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
