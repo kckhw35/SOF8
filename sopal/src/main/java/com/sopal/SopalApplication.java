ac7f4e433d3851d9b697797047efd244183b8ea5
Hyewon <kckhw35@naver.com>
2023-01-13 11:39:34 +0900

    20230113	Hyewon		test project name

---
+++ sopal/src/main/java/com/sopal/SopalApplication.java
@@ -0,0 +1,13 @@
+package com.sopal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopalApplication.class, args);
+	}
+
+}
---
+++ sopal/src/test/java/com/sopal/SopalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sopal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
